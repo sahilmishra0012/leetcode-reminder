039b53f1f418896a80055afda4495a329b8ae57d
Sahil Mishra <sahilmishra0012@outlook.com>
2022-02-13 00:16:47 +0530

    Added script to load config

---
+++ load_config.py
@@ -0,0 +1,12 @@
+import configparser
+
+def load_config():
+
+    parser = configparser.ConfigParser()    
+    parser.read('/home/samkiller007/leetcode/config')   
+    
+    config_json = {}
+    for sect in parser.sections():
+        for k,v in parser.items(sect):
+            config_json[k] = v
+    return config_json

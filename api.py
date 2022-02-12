06ac96e033285763c4a97947aadbcc632efbdb2c
Sahil Mishra <sahilmishra0012@outlook.com>
2022-02-13 00:16:59 +0530

    Added API script

---
+++ api.py
@@ -0,0 +1,21 @@
+import requests
+import json
+from load_config import load_config
+from get_time import get_time
+
+def get_current_submissions():
+    config = load_config()
+    current_date = str(get_time(config))
+
+    data = {"query": config["query"], "variables":{"username":config["username"]}}
+    headers = {"Content-Type": config["contenttype"], "referer" : config["referer"]}
+
+    response = requests.post(config["url"], json=data, headers=headers)
+    
+    status = response.json()["data"]["matchedUser"]["submissionCalendar"]
+    status = json.loads(status)
+
+    if current_date in status.keys():
+        return status[current_date]
+    else:
+        return 0

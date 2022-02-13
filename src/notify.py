f12f3e08a191dd95226012141d812ccf494ab329
Sahil Mishra <sahilmishra0012@outlook.com>
2022-02-13 11:48:10 +0530

    Updated build to consume absolute paths

--- src/notify.py
+++ src/notify.py
@@ -9,20 +9,20 @@ while(True):
         notification.notify(
             title = "LeetCode Statistics on {}".format(datetime.date.today()),
             message = "You have not made any submissions today!",
-            app_icon = "../res/icons/bell-icon.ico",
+            # app_icon = "/home/samkiller007/leetcode/res/icons/bell-icon.ico",
             timeout  = notificationduration)
-    else:
-        if(submission_count==1):
-            notification.notify(
-                title = "LeetCode Statistics on {}".format(datetime.date.today()),
-                message = "Yeyy!! You have made 1 submission today!",
-                app_icon = "../res/icons/bell-icon.ico",
-                timeout  = notificationduration)
-        else:
-            notification.notify(
-                title = "LeetCode Statistics on {}".format(datetime.date.today()),
-                message = "Yeyy!! You have made {} submissions today!".format(submission_count),
-                app_icon = "../res/icons/bell-icon.ico",
-                timeout  = notificationduration)
+    # else:
+    #     if(submission_count==1):
+    #         notification.notify(
+    #             title = "LeetCode Statistics on {}".format(datetime.date.today()),
+    #             message = "Yeyy!! You have made 1 submission today!",
+    #             app_icon = "/home/samkiller007/leetcode/res/icons/bell-icon.ico",
+    #             timeout  = notificationduration)
+    #     else:
+    #         notification.notify(
+    #             title = "LeetCode Statistics on {}".format(datetime.date.today()),
+    #             message = "Yeyy!! You have made {} submissions today!".format(submission_count),
+    #             app_icon = "/home/samkiller007/leetcode/res/icons/bell-icon.ico",
+    #             timeout  = notificationduration)
 
     time.sleep(remindinterval)

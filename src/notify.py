9206668b2956a7bbd8552153029ce56f621251ba
Sahil Mishra <sahilmishra0012@outlook.com>
2022-02-13 10:21:30 +0530

    Updated relative paths

--- src/notify.py
+++ src/notify.py
@@ -9,20 +9,20 @@ while(True):
         notification.notify(
             title = "LeetCode Statistics on {}".format(datetime.date.today()),
             message = "You have not made any submissions today!",
-            app_icon = "Paomedia-Small-N-Flat-Bell.ico",
+            app_icon = "../res/icons/bell-icon.ico",
             timeout  = 10)
     else:
         if(submission_count==1):
             notification.notify(
                 title = "LeetCode Statistics on {}".format(datetime.date.today()),
                 message = "Yeyy!! You have made 1 submission today!",
-                app_icon = "Paomedia-Small-N-Flat-Bell.ico",
+                app_icon = "../res/icons/bell-icon.ico",
                 timeout  = 10)
         else:
             notification.notify(
                 title = "LeetCode Statistics on {}".format(datetime.date.today()),
                 message = "Yeyy!! You have made {} submissions today!".format(submission_count),
-                app_icon = "Paomedia-Small-N-Flat-Bell.ico",                
+                app_icon = "../res/icons/bell-icon.ico",
                 timeout  = 10)
 
     time.sleep(60)

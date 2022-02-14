988a877f009034ee1a9d3cccddea781bd64dbf54
Sahil Mishra <sahilmishra0012@outlook.com>
2022-02-14 11:49:55 +0530

    Added network connection support

--- src/notify.py
+++ src/notify.py
@@ -2,27 +2,39 @@ from plyer import notification
 import time
 import datetime
 import api
+from load_config import load_config
+from network import check_connection
 
 while(True):
-    submission_count, remindinterval, notificationduration = api.get_current_submissions()
-    if(submission_count==0):
+    is_connected = check_connection()
+    config = load_config()
+    remindinterval = int(config["remindinterval"])
+    notificationduration = int(config["notificationduration"])
+    if(is_connected==True):
+        submission_count = api.get_current_submissions()
+        if(submission_count==0):
+            notification.notify(
+                title = "LeetCode Statistics on {}".format(datetime.date.today()),
+                message = "You have not made any submissions today!",
+                # app_icon = "/home/samkiller007/leetcode/res/icons/bell-icon.ico",
+                timeout  = notificationduration)
+        else:
+            if(submission_count==1):
+                notification.notify(
+                    title = "LeetCode Statistics on {}".format(datetime.date.today()),
+                    message = "Yeyy!! You have made 1 submission today!",
+                    timeout  = notificationduration)
+                break
+            else:
+                notification.notify(
+                    title = "LeetCode Statistics on {}".format(datetime.date.today()),
+                    message = "Yeyy!! You have made {} submissions today!".format(submission_count),
+                    timeout  = notificationduration)
+                break
+        time.sleep(remindinterval)
+    else:
         notification.notify(
-            title = "LeetCode Statistics on {}".format(datetime.date.today()),
-            message = "You have not made any submissions today!",
-            # app_icon = "/absolute/path/to/res/icons/bell-icon.ico",
-            timeout  = notificationduration)
-    # else:
-    #     if(submission_count==1):
-    #         notification.notify(
-    #             title = "LeetCode Statistics on {}".format(datetime.date.today()),
-    #             message = "Yeyy!! You have made 1 submission today!",
-    #             app_icon = "/absolute/path/to/res/icons/bell-icon.ico",
-    #             timeout  = notificationduration)
-    #     else:
-    #         notification.notify(
-    #             title = "LeetCode Statistics on {}".format(datetime.date.today()),
-    #             message = "Yeyy!! You have made {} submissions today!".format(submission_count),
-    #             app_icon = "/absolute/path/to/res/icons/bell-icon.ico",
-    #             timeout  = notificationduration)
-
-    time.sleep(remindinterval)
+                title = "LeetCode Statistics - Network Error",
+                message = "Connect to internet!",
+                timeout  = notificationduration)
+        time.sleep(remindinterval)
